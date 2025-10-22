a26ba37e03cbd2a526fe977f2fed20f5f5dbbef9
itc-s24006 <s24006@std.it-college.ac.jp>
2025-10-22 15:21:51 +0900

    超基本のselect文実行できた

--- ex-gen-app/src/routes/hello.ts
+++ ex-gen-app/src/routes/hello.ts
@@ -1,35 +1,29 @@
 import { Router } from 'express'
+import mariadb from 'mariadb'
 
 const router = Router()
+const db = await mariadb.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'example',
+    database: 'mydb',
+})
 
-declare module 'express-session' {
-    interface SessionData {
-        message?: string
-    }
+//型定義
+interface MyData {
+    id: number
+    name: string
+    mail: string
+    age: number
 }
 
 router.get('/', async (req, res, next) => {
-    const msg = req.session.message !== undefined
-    ? `Last Message: ${req.session.message}`
-        : "※何か書いて送信してください。"
-
-
-    const data = {
-        title: 'Hello',
-        content: msg
-    }
-    //  第1引数:テンプレートファイル　第2引数:レンダリング用のパラメーター
-    res.render('hello', data)
-})
-
-router.post('/post', async (req, res, next) => {
-    const msg = req.body.message as string | undefined
-    req.session.message = msg
-    const data = {
+    const result = await db.query<MyData[]>('SELECT * FROM mydata')
+    res.render('hello', {
         title: 'Hello!',
-        content: `あなたは「${msg}」と送信しました。`
-    }
-    res.render('hello', data)
+        content: result
+    })
 })
 
 export default router
