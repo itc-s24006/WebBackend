17fc264e0ddf2d632f6b66bb7fdc51bfd2ec7f8e
itc-s24006 <s24006@std.it-college.ac.jp>
2025-10-29 16:26:02 +0900

    名前検索、複数の条件指定 リスト6-13p337　までできた

--- ex-gen-app/src/routes/users.ts
+++ ex-gen-app/src/routes/users.ts
@@ -15,15 +15,36 @@ const prisma = new PrismaClient({adapter})
 
 interface UserParams {
     id?: string
+    name?: string
+    min?: string
+    max?: string
 }
 
 router.get('/', async (req: Request<{}, {}, {}, UserParams>, res, next) => {
     const id = parseInt(req.query.id || '') // 「|| ''」 いろんな型が存在するからparseIntが受け取れる型(string)を明示して書く
-    const users = await (id ? prisma.user.findMany({where: {id}}) // findMany()  Userテーブルのオブジェクトを返す
+    const users = await (id ? prisma.user.findMany({where: {id: {lte: id}}}) // findMany() Userテーブルのオブジェクトを返す
+        // {id} = {id: id}省略してる  lteは<=(以下)と同じ
         : prisma.user.findMany())
 
     res.render('users/index', {
         title: 'Users/Index',
+        content: users,
+    })
+})
+
+router.get('/find', async (req: Request<{}, {}, {}, UserParams>, res, next) => {
+    const {name} = req.query
+    const min = parseInt(req.query.min || '')
+    const max = parseInt(req.query.max || '')
+
+    const users= await prisma.user.findMany({where: {
+        AND: [
+            {nama: {contains: name}},
+            {age: {gte: min, lte: max}}
+        ]
+        }})
+    res.render('users/index', {
+        title: 'Users/Find',
         content: users,
     })
 })
