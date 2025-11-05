4977eba97eb904fd75413e42ab2f080c2dae3092
itc-s24006 <s24006@std.it-college.ac.jp>
2025-11-05 15:51:15 +0900

    ソートとページネーションできた

--- ex-gen-app/src/routes/users.ts
+++ ex-gen-app/src/routes/users.ts
@@ -19,13 +19,22 @@ interface UserParams {
     min?: string
     max?: string
     mail?: string
+    page?: string
 }
 
+const PAGE_SIZE = 3
+
 router.get('/', async (req: Request<{}, {}, {}, UserParams>, res, next) => {
-    const id = parseInt(req.query.id || '') // 「|| ''」 いろんな型が存在するからparseIntが受け取れる型(string)を明示して書く
-    const users = await (id ? prisma.user.findMany({where: {id: {lte: id}}}) // findMany() Userテーブルのオブジェクトを返す
-        // {id} = {id: id}省略してる  lteは<=(以下)と同じ
-        : prisma.user.findMany())
+    // 0よりも大きければその数値を、そうでなければ1を設定
+    const page = req.query.page && parseInt(req.query.page) > 0 ?
+        parseInt(req.query.page) : 1
+    const users = await prisma.user.findMany({
+        orderBy: [
+            {id: 'asc'}
+        ],
+        skip: (page - 1) * PAGE_SIZE,
+        take: PAGE_SIZE,
+    })
 
     res.render('users/index', {
         title: 'Users/Index',
